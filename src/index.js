61607313945d5c4b4519072ba5f87e2154808b43
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-02-21 12:42:55 +0600

    Minor Edit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import StarRating from "./components/Main/StartRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10} />
+    <App />
   </React.StrictMode>
 );
